e72289a838b0bf88d808a0172b785eb22028b359
Saurabh Misra <saurabhmisra87@gmail.com>
2019-05-21 13:21:38 +0530

    Modify post footer author section to include photo and contacts and new styling.

--- src/components/Post/Author/Author.js
+++ src/components/Post/Author/Author.js
@@ -1,25 +1,32 @@
 // @flow
-import React from 'react';
-import { getContactHref } from '../../../utils';
-import styles from './Author.module.scss';
-import { useSiteMetadata } from '../../../hooks';
+import React from "react";
+import { withPrefix, Link } from "gatsby";
+import { getContactHref } from "../../../utils";
+import styles from "./Author.module.scss";
+import { useSiteMetadata } from "../../../hooks";
+import Contacts from "../../Sidebar/Contacts";
 
 const Author = () => {
   const { author } = useSiteMetadata();
 
   return (
-    <div className={styles['author']}>
-      <p className={styles['author__bio']}>
-        {author.bio}
-        <a
-          className={styles['author__bio-twitter']}
-          href={getContactHref('twitter', author.contacts.twitter)}
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <strong>{author.name}</strong> on Twitter
-        </a>
-      </p>
+    <div className={styles["author"]}>
+      <Link to="/">
+        <img
+          src={withPrefix(author.photo)}
+          className={styles["author__photo"]}
+          width="75"
+          height="75"
+          alt={author.name}
+        />
+      </Link>
+      <div className={styles["author__info"]}>
+        <Link className={styles["author__title-link"]} to="/">
+          <strong>{author.name}</strong>
+        </Link>
+        <p className={styles["author__bio"]}>{author.bio}</p>
+        <Contacts contacts={author.contacts} />
+      </div>
     </div>
   );
 };
