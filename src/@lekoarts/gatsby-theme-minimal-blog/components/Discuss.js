c68257bf856f713481355ceb3bdc52abb7807260
Saurabh Misra <saurabhmisra.dev@gmail.com>
2023-09-07 19:17:25 +0530

    Add alt attributes to img tags. Also disable the Discuss on Twitter link for now.

--- src/@lekoarts/gatsby-theme-minimal-blog/components/Discuss.js
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/Discuss.js
@@ -1,11 +1,13 @@
-import React from 'react';
+// import React from 'react';
 
 export default function SocialSharing() {
-  const link = typeof window !== "undefined" ? `https://twitter.com/search?q=${ encodeURIComponent( window.location.href ) }` : null;
+  // const link = typeof window !== "undefined" ? `https://twitter.com/search?q=${ encodeURIComponent( window.location.href ) }` : null;
 
-  return (
-    <p className="discuss-on-twitter">
-      <a href={link} target="_blank">Discuss on Twitter</a>
-    </p>
-  )
+  // return (
+  //   <p className="discuss-on-twitter">
+  //     <a href={link} target="_blank">Discuss on Twitter</a>
+  //   </p>
+  // )
+
+  return null;
 }
